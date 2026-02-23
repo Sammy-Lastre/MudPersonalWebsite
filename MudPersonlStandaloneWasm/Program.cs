0021b9568bb8b572fc3859a36b8d6089b52135a9
SamLS42 <sammyls@protonmail.com>
2026-02-23 20:26:14 +0100

    adds standalone app

---
+++ MudPersonlStandaloneWasm/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using MudPersonlStandaloneWasm;
+
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddMudServices();
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
